4f3fab4f6a23711100e429a7a9fdf112ce9aeb3a
ayan-011 <ayansaifi2nd@gmail.com>
2026-09-03 21:28:28 +0530

    generate skills page

---
+++ myapp/app/pages/Skills.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='py-10 bg-  w-full h-screen flex justify-center'>
+         
+        <div className="min-w-[60vw] items-center     p-1 mt-20" >
+             <span className='text-4xl font-semibold '>SKILLS</span>
+
+             <div className="skills grid grid-cols-3 grid-rows-4 gap-5 gap-y-14 py-32">
+
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+              <div className="  rounded  flex p-2 border-1 border-white items-center justify-between">
+                   <img src="/skills/js.png" alt="" className='w-12 rounded' />
+                   <span className='text-3xl  w-full flex justify-center  tracking-tight' >Javascript</span>
+              </div>
+             
+               
+             </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
